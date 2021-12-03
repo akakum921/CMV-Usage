85fc557498b9c11ae6b6997f4c5649de218b03f1
AKASH KUMAR SINGH <as5413@srmist.edu.in>
2021-12-03 21:09:37 +0530

    Updated the manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
+// const user = require('../models/user');
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('post',{
-        title: 'Post'
-    });
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err,user){
+           if(user){
+               return res.render('user_profile',{
+                   title: 'User Profile',
+                   user: user
+               })
+           }else{
+               return res.redirect('/users/sign-in');
+           }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
+
 }
 
 
