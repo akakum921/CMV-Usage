0cb8139607de9b740d0fe8b40a959bc8c889bcf8
AKASH KUMAR SINGH <as5413@srmist.edu.in>
2021-12-13 17:01:06 +0530

    Converting controller codes to async await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,41 +1,43 @@
-const Comment = require('../models/comment');
-const Post = require('../models/post');
-module.exports.create = function(req,res){
-    Post.findById(req.body.post,function(err,post){
-         
-        if(post){
-            Comment.create({
-                content: req.body.content,
-                post: req.body.post,
-                user: req.user._id
-            },function(err,comment){
-                if(err){
-                    console.log('error in creating the comment'); return; }
-                  
-                post.comments.push(comment);    
-                post.save();
+const Comment = require("../models/comment");
+const Post = require("../models/post");
+module.exports.create = async function (req, res) {
+  try {
+    let post = await Post.findById(req.body.post);
 
+    if (post) {
+      let comment = await Comment.create({
+        content: req.body.content,
+        post: req.body.post,
+        user: req.user._id,
+      });
+      post.comments.push(comment);
+      post.save();
 
-                return res.redirect('/');
-            });
-        }
-    });
-} 
+      return res.redirect("/");
+    }
+  } catch (err) {
+    console.log("error in creating the comment");
+    return;
+  }
+  return;
+};
 
 //to delete a comment
-module.exports.destroy = function(req,res) {
-      
-    Comment.findById(req.params.id, function(err,comment){
-            if(comment.user == req.user.id) {
+module.exports.destroy = async function (req, res) {
+  try {
+    let comment = await Comment.findById(req.params.id);
 
-                let postId = comment.post;
-                comment.remove();
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+      comment.remove();
 
-                Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}}, function(err,post){
-                    return res.redirect('back');
-                });
-            }else {
-                return res.redirect('back');
-            }
-    });
-}
+      let post = Post.findByIdAndUpdate(postId, {$pull: { comments: req.params.id }});
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (err) {
+      console.log("Error in deleting the comment",err);
+      return;
+  }
+};
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,36 +1,32 @@
-const Post = require('../models/post');
-const Comment = require('../models/comment');
-module.exports.create = function(req,res){
+const Post = require("../models/post");
+const Comment = require("../models/comment");
+module.exports.create = async function (req, res) {
+  try {
     Post.create({
-        content: req.body.content,
-        user: req.user._id
-    }, function(err,post){
-        if(err)
-        {
-           console.log('Error in creating a post'); 
-           return;
-        }
-        
-        return res.redirect('back');
+      content: req.body.content,
+      user: req.user._id,
     });
-    return res.redirect('/');
-
-}
+    return res.redirect("/");
+  } catch (err) {
+    console.log("Error in creating a post",err);
+    return;
+  }
+};
 
 //to delete a post
-module.exports.destroy = function(req,res){
+module.exports.destroy = async function (req, res) {
+  try {
+    let post = await Post.findById(req.params.id);
+    if (post.user == req.user.id) {
+      post.remove();
 
-    Post.findById(req.params.id, function(err,post){
-        // .id means converting the object id into string
-        if(post.user == req.user.id){
-            post.remove();
-            
-            Comment.deleteMany({post: req.params.id}, function(err){
-                   return res.redirect('/');
-            });
-        }else{
-            return res.redirect('back');
-        }
-
-    });
-}
+      await Comment.deleteMany({ post: req.params.id });
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (err) {
+    console.log("Error in deleting a post",err);
+    return;
+  }
+};
