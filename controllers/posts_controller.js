2f6073b36e3ae2f530074812612a8e496931a143
AKASH KUMAR SINGH <as5413@srmist.edu.in>
2021-12-14 22:23:29 +0530

    Added flash messages to other sections

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,14 +2,16 @@ const Post = require("../models/post");
 const Comment = require("../models/comment");
 module.exports.create = async function (req, res) {
   try {
-    Post.create({
+    await Post.create({
       content: req.body.content,
       user: req.user._id,
     });
+    req.flash('success','Post Published!');
     return res.redirect("/");
   } catch (err) {
-    console.log("Error in creating a post",err);
-    return;
+    // console.log("Error in creating a post",err);
+    req.flash('error',err);
+    return res.redirect("/");
   }
 };
 
@@ -21,12 +23,15 @@ module.exports.destroy = async function (req, res) {
       post.remove();
 
       await Comment.deleteMany({ post: req.params.id });
+      req.flash('success','Post & associated comments deleted');
       return res.redirect("back");
     } else {
-      return res.redirect("back");
+      req.flash('error','You cannot delete this post');
+      return res.redirect('back');
     }
   } catch (err) {
-    console.log("Error in deleting a post",err);
-    return;
+    // console.log("Error in deleting a post",err);
+    req.flash('error',err);
+    return res.redirect('back');
   }
 };
